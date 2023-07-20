384de77ffcf086f4fdaf1cc31b6dbd9da206e81d
copy-ninja1 <emmajnr1000@gmail.com>
2023-07-20 04:51:06 +0100

    fix: auth

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "./schema";
+
 export default function Login() {
+     const {
+          register,
+          handleSubmit,
+          formState: { errors },
+          reset,
+     } = useForm({
+          resolver: yupResolver(schema),
+     });
+     const onSubmit = (data) => {
+          console.log(data);
+     };
+
      return (
           <div className="flex w-screen h-screen items-center justify-center">
                <div className="bg-gray-50 p-6 border rounded-xl w-full md:w-1/2 xl:w-1/3">
@@ -6,29 +23,37 @@ export default function Login() {
                          <h2 className="title">Login</h2>
                     </div>
 
-                    <form action="#">
-                         <label htmlFor="login-email">
-                              Username or email address
-                              <span className="required">*</span>
-                         </label>
-                         <input
-                              type="email"
-                              className="form-input form-wide"
-                              id="login-email"
-                              required
-                         />
-
-                         <label htmlFor="login-password">
-                              Password
-                              <span className="required">*</span>
-                         </label>
-                         <input
-                              type="password"
-                              className="form-input form-wide"
-                              id="login-password"
-                              required
-                         />
-
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                         <div className="mb-1">
+                              <label htmlFor="login-email">
+                                   Username or email address
+                                   <span className="required">*</span>
+                              </label>
+                              <input
+                                   type="email"
+                                   {...register("email")}
+                                   className={`${
+                                        errors.email?.message && "border-red-500"
+                                   } form-input form-wide`}
+                                   id="login-email"
+                              />
+                              <p className="text-red-500 text-base">{errors.email?.message}</p>
+                         </div>
+                         <div className="mb-1">
+                              <label htmlFor="login-password">
+                                   Password
+                                   <span className="required">*</span>
+                              </label>
+                              <input
+                                   type="password"
+                                   {...register("password")}
+                                   className={`${
+                                        errors.email?.message && "border-red-500"
+                                   } form-input form-wide`}
+                                   id="login-password"
+                              />
+                              <p className="text-red-500 text-base">{errors.password?.message}</p>
+                         </div>
                          <div className="form-footer">
                               <div className="custom-control custom-checkbox mb-0">
                                    <input
--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "./schema";
+
 export default function Register() {
+     const {
+          register,
+          handleSubmit,
+          formState: { errors },
+          reset,
+     } = useForm({
+          resolver: yupResolver(schema),
+     });
+
+     const onSubmit = (data) => {
+          console.log(data);
+     };
+
      return (
           <div className="flex w-screen h-screen items-center justify-center">
                <div className="bg-gray-50 p-6 border rounded-xl w-full md:w-1/2 xl:w-1/3">
@@ -6,31 +24,42 @@ export default function Register() {
                          <h2 className="title">Register</h2>
                     </div>
 
-                    <form action="#">
-                         <label htmlFor="register-email">
-                              Email address
-                              <span className="required">*</span>
-                         </label>
-                         <input
-                              type="email"
-                              className="form-input form-wide"
-                              id="register-email"
-                              required
-                         />
-
-                         <label htmlFor="register-password">
-                              Password
-                              <span className="required">*</span>
-                         </label>
-                         <input
-                              type="password"
-                              className="form-input form-wide"
-                              id="register-password"
-                              required
-                         />
-
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                         <div className="mb-1">
+                              <label htmlFor="register-email">
+                                   Email address
+                                   <span className="required">*</span>
+                              </label>
+                              <input
+                                   type="email"
+                                   {...register("email")}
+                                   className={`${
+                                        errors.email?.message && "border-red-500"
+                                   } form-input form-wide`}
+                                   id="register-email"
+                              />
+                              <p className="text-red-500 text-base">{errors.email?.message}</p>
+                         </div>
+                         <div className="mb-1">
+                              <label htmlFor="register-password">
+                                   Password
+                                   <span className="required">*</span>
+                              </label>
+                              <input
+                                   type="password"
+                                   {...register("password")}
+                                   className={`${
+                                        errors.email?.message && "border-red-500"
+                                   } form-input form-wide`}
+                                   id="register-password"
+                              />
+                              <p className="text-red-500 text-base">{errors.password?.message}</p>
+                         </div>
                          <div className="form-footer mb-2">
-                              <button type="submit" className="btn btn-dark btn-md w-100 mr-0">
+                              <button
+                                   type="submit"
+                                   className="btn bg-gray-800 text-gray-50 hover:bg-gray-700 rounded-sm btn-md w-100"
+                              >
                                    Register
                               </button>
                          </div>
