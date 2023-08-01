92264e2533342a8bca647d6c38a0734087d82685
Ahmedoo <ahmedootech@gmail.com>
2023-08-01 01:35:08 +0100

    Done with Theme5 Demo1

--- src/templates/config.ts
+++ src/templates/config.ts
@@ -1,11 +1,13 @@
 import Theme1Header from "./Theme1/Header/Header";
 import Theme2Header from "./Theme2/Header/Header";
 import Theme3Header from "./Theme3/Header/Header";
+import Theme5Header from "./Theme5/Header/Header";
 
 export const templateConfig: any = {
      headers: {
           theme1: Theme1Header,
           theme2: Theme2Header,
           theme3: Theme3Header,
+          theme5: Theme5Header,
      },
 };
--- src/templates/config/index.ts
+++ src/templates/config/index.ts
@@ -1,7 +1,9 @@
 import { theme1TemplateConfig } from "./theme1";
 import { theme2TemplateConfig } from "./theme2";
+import { theme5TemplateConfig } from "./theme5";
 
 export const templateConfig: any = {
-    theme1: theme1TemplateConfig,
-    theme2: theme2TemplateConfig
+     theme1: theme1TemplateConfig,
+     theme2: theme2TemplateConfig,
+     theme5: theme5TemplateConfig,
 };
