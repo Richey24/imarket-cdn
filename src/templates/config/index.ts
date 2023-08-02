3de21614aad4979a0fcafe6d23eeb18e0a4c630c
copy-ninja1 <emmajnr1000@gmail.com>
2023-08-02 18:51:37 +0100

    yarn install

--- src/templates/config/index.ts
+++ src/templates/config/index.ts
@@ -1,7 +1,6 @@
 import { theme1TemplateConfig } from "./theme1";
 import { theme2TemplateConfig } from "./theme2";
 import { theme5TemplateConfig } from "./theme5";
-import { theme6TemplateConfig } from "./theme6";
 import { theme3TemplateConfig } from "./theme3";
 import { theme6TemplateConfig } from "./theme6";
 
