99ddbc7db7780f8b5d945fb724eb37292bbf09e4
olaiya <olaiyabasit@yahoo.com>
2023-08-03 21:19:20 +0100

    added products component

--- src/templates/Theme4/Home/data.ts
+++ src/templates/Theme4/Home/data.ts
@@ -17,3 +17,80 @@ export const slidesData = [
           buttonLink: "category.html",
      },
 ];
+
+import product5 from "../../../assets/images/demoes/demo13/products/product-5.jpg";
+import product1 from "../../../assets/images/demoes/demo13/products/product-1.jpg";
+import product2 from "../../../assets/images/demoes/demo13/products/product-2.jpg";
+import product3 from "../../../assets/images/demoes/demo13/products/product-3.jpg";
+import product4 from "../../../assets/images/demoes/demo13/products/product-4.jpg";
+import product6 from "../../../assets/images/demoes/demo13/products/product-6.jpg";
+import product10 from "../../../assets/images/demoes/demo13/products/product-10.jpg";
+import product11 from "../../../assets/images/demoes/demo13/products/product-11.jpg";
+import product12 from "../../../assets/images/demoes/demo13/products/product-12.jpg";
+
+export const latestProducts = [
+     {
+          category: "category",
+          imageUrl: product1,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 3,
+     },
+     {
+          category: "category",
+          imageUrl: product2,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 23,
+     },
+     {
+          category: "category",
+          imageUrl: product3,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 9393,
+     },
+     // Add more latest products as needed
+     {
+          category: "category",
+          imageUrl: product4,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 383,
+     },
+     {
+          category: "category",
+          imageUrl: product5,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 930,
+     },
+     {
+          category: "category",
+          imageUrl: product6,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 123,
+     },
+     {
+          category: "category",
+          imageUrl: product10,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 324,
+     },
+     {
+          category: "category",
+          imageUrl: product11,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 903,
+     },
+     {
+          category: "category",
+          imageUrl: product12,
+          title: "Product Short Name",
+          price: 49,
+          oldPrice: 38,
+     },
+];
