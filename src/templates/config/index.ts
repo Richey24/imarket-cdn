0ac476242776831f7af9f38998c183b41629913d
Rejoice <uahomorejoice@gmail.com>
2023-08-07 17:33:35 +0100

    theme 5

--- src/templates/config/index.ts
+++ src/templates/config/index.ts
@@ -3,33 +3,22 @@ import { theme2TemplateConfig } from "./theme2";
 import { theme5TemplateConfig } from "./theme5";
 import { theme6TemplateConfig } from "./theme6";
 import { theme3TemplateConfig } from "./theme3";
-<<<<<<< HEAD
-import { theme8TemplateConfig } from "./theme8";
-=======
 import { theme7TemplateConfig } from "./theme7";
 import { theme10TemplateConfig } from "./theme10";
 import { theme11TemplateConfig } from "./theme11";
->>>>>>> 13e58ac4e3f8a9683ab362a4c05ea56c12eddedf
+import { theme8TemplateConfig } from "./theme8";
 
 // Theme5 demo1
 // Theme6 demo2
 // Theme7 demo3
 export const templateConfig: any = {
-<<<<<<< HEAD
-     theme1: theme1TemplateConfig,
-     theme2: theme2TemplateConfig,
-     theme3: theme3TemplateConfig,
-     theme5: theme5TemplateConfig,
-     theme6: theme6TemplateConfig,
-     theme8: theme8TemplateConfig
-=======
     theme1: theme1TemplateConfig,
     theme2: theme2TemplateConfig,
     theme3: theme3TemplateConfig,
     theme5: theme5TemplateConfig,
     theme6: theme6TemplateConfig,
     theme7: theme7TemplateConfig,
+    theme8: theme8TemplateConfig,
     theme10: theme10TemplateConfig,
     theme11: theme11TemplateConfig,
->>>>>>> 13e58ac4e3f8a9683ab362a4c05ea56c12eddedf
 };
