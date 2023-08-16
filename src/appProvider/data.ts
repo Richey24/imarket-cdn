ca7076393a77c3a403784fd01dc26850d55167cd
KELEX <ogkelvin99@gmail.com>
2023-08-16 04:47:15 +0100

    theme14

--- src/appProvider/data.ts
+++ src/appProvider/data.ts
@@ -1,11 +1,11 @@
 export const dummySite = {
      theme: {
-          theme: "theme14",
+          theme: "theme12",
           footer: {
                name: "footer",
                content: "",
                component: {
-                    theme: "theme14",
+                    theme: "theme12",
                     name: "",
                     props: {
                          static: {
@@ -36,7 +36,7 @@ export const dummySite = {
                name: "header",
                content: "",
                component: {
-                    theme: "theme14",
+                    theme: "theme12",
                     props: {
                          company: {},
                          static: {
@@ -77,7 +77,7 @@ export const dummySite = {
                               name: "home",
                               content: "",
                               component: {
-                                   theme: "theme14",
+                                   theme: "theme12",
                                    props: {
                                         static: {
                                              promotion: {
