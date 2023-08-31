c2bc38872dc2219eb6cc38dc32d2af9dd7a4d685
Ahmedoo <ahmedootech@gmail.com>
2023-08-31 01:52:30 +0100

    conflict fixed

--- src/templates/config/index.ts
+++ src/templates/config/index.ts
@@ -18,6 +18,7 @@ import theme20TemplateConfig from "./theme20";
 import theme21TemplateConfig from "./theme21";
 import theme22TemplateConfig from "./theme22";
 import theme25TemplateConfig from "./theme25";
+import theme27TemplateConfig from "./theme27";
 import theme30TemplateConfig from "./theme30";
 import theme31TemplateConfig from "./theme31";
 import theme32TemplateConfig from "./theme32";
@@ -53,6 +54,7 @@ export const templateConfig: any = {
      theme21: theme21TemplateConfig,
      theme22: theme22TemplateConfig,
      theme25: theme25TemplateConfig,
+     theme27: theme27TemplateConfig,
      theme30: theme30TemplateConfig,
      theme31: theme31TemplateConfig,
      theme32: theme32TemplateConfig,
--- src/templates/config/theme27.ts
+++ src/templates/config/theme27.ts
@@ -2,8 +2,10 @@ import T27Footer from "../Theme27/Footer/Footer";
 import T27Header from "../Theme27/Header/Header";
 import { Home } from "../Theme27/Home/Home";
 
-export const theme27TemplateConfig = {
+const theme27TemplateConfig = {
      header: T27Header,
      home: Home,
      footer: T27Footer,
 };
+
+export default theme27TemplateConfig;
