8f7a54430d5524e651968ff9ac8b908f120a4d27
Viktoh-Ikenna <viktoh675@gmail.com>
2023-10-11 02:49:22 +0100

    latest resolver

--- src/app/(pages)/[product]/page.tsx
+++ src/app/(pages)/[product]/page.tsx
@@ -1,8 +1,6 @@
 import { findFeaturedProducts, findProduct } from "@/api/product.api";
 import { ProductDescription } from "@/app/components/Product/Product-description";
 import ProductStuf from "@/app/components/Product/product-stuf";
-import { useNumberInput } from "@chakra-ui/number-input";
-// import { Button, HStack, Input, useNumberInput } from "@chakra-ui/react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -15,23 +13,6 @@ async function ProductPage({ params }: { params: { product: string } }) {
           if (!product) return notFound();
      }
 
-     // const featuredProducts = findFeaturedProducts();
-
-     // // console.log({ props });
-     // const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } = useNumberInput({
-     //      step: 0.01,
-     //      defaultValue: 1.53,
-     //      min: 1,
-     //      max: 6,
-     //      precision: 2,
-     // });
-
-     // const inc = getIncrementButtonProps();
-     // const dec = getDecrementButtonProps();
-     // const input = getInputProps();
-
-     // const img =
-     //      "https://images.unsplash.com/photo-1689598843935-1aba53dd1277?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
      return (
           <div className="tw-pb-12 tw-pt-8">
                <nav aria-label="breadcrumb" className="breadcrumb-nav">
